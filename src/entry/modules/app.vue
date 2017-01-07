d148d04059361643a5774890dec6ced2e55ebdaf
李昕亮 <lixinliang@yy.com>
2017-01-08 00:46:07 +0800

    update
    update

---
+++ src/entry/modules/app.vue
@@ -0,0 +1,50 @@
+<script>
+import tabbar from './tabbar.vue';
+
+export default {
+    components : {
+        tabbar,
+    },
+    beforeCreate () {
+        console.log('beforeCreate');
+    },
+    created () {
+        console.log('created');
+    },
+    beforeMount () {
+        console.log('beforeMount');
+    },
+    mounted () {
+        console.log('mounted');
+    },
+    beforeUpdate () {
+        console.log('beforeUpdate');
+    },
+    updated () {
+        console.log('updated');
+    },
+    activated () {
+        console.log('activated');
+    },
+    deactivated () {
+        console.log('deactivated');
+    },
+    beforeDestroy () {
+        console.log('beforeDestroy');
+    },
+    destroyed () {
+        console.log('destroyed');
+    },
+}
+</script>
+
+<template>
+	<div class="app">
+        <router-view></router-view>
+		<tabbar></tabbar>
+	</div>
+</template>
+
+<style lang="scss">
+	@import "../sass/variable.scss";
+</style>
