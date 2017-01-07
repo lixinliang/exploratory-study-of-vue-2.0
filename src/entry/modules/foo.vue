9a51e995100fd2b0cc395aa36d2fea389d0ea0da
李昕亮 <lixinliang@yy.com>
2017-01-08 00:46:16 +0800

    update
    update

---
+++ src/entry/modules/foo.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+	data () {
+		return {
+			title : 'foo',
+		};
+	},
+	computed : {
+		counter () {
+			return this.$store.state.foo_page_counter;
+		},
+	},
+	beforeCreate () {
+        this.$store.state.foo_page_counter++;
+    },
+};
+</script>
+
+<template>
+	<div class="foo">
+		<div class="title">{{ title }}</div>
+		<div class="counter">{{ counter }}</div>
+	</div>
+</template>
+
+<style lang="scss">
+	@import "../sass/variable.scss";
+	.foo {
+		color: $text-color;
+	}
+</style>
