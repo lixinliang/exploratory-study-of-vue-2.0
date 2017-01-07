3a9ee15775adc18418ca5774bdcaef1b8246c4f8
李昕亮 <lixinliang@yy.com>
2017-01-08 00:46:11 +0800

    update
    update

---
+++ src/entry/modules/bar.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+	data () {
+		return {
+			title : 'bar',
+		};
+	},
+	computed : {
+		counter () {
+			return this.$store.state.bar_page_counter;
+		},
+	},
+	beforeCreate () {
+        this.$store.state.bar_page_counter++;
+    },
+};
+</script>
+
+<template>
+	<div class="bar">
+		<div class="title">{{ title }}</div>
+		<div class="counter">{{ counter }}</div>
+	</div>
+</template>
+
+<style lang="scss">
+	@import "../sass/variable.scss";
+	.bar {
+		color: $text-color;
+	}
+</style>
